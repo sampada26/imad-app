1d2a48b2fd0ce1a0179e39895aa1aa101e4ae50b
sampada26 <31102844+sampada26@users.noreply.github.com>
2017-08-20 19:11:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ button.onclick = function() {
   request.open('GET', 'http://sampada966.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+// submit name
+var nameInput = document.getElementById("name");
+var nameInput,value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function() {
+    // Make a request to the server and send the name
+    //capture the list of names and render it as a list
+    var names = ["name1","name2", "name3", "name4"];
+    var list = '';
+    for (var i=0; i< name.length; i++) {
+        list += <li> + names[i] + </li>;
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
